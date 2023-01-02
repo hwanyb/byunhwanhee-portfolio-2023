7b4162f81f07ef0248c179b7aabee038ffb2937c
hwanyb <mnn9502@naver.com>
2023-01-02 11:50:00 +0900

    Design : Setting theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,42 @@
+import { DefaultTheme } from "styled-components";
+
+const windowSize = {
+  small: `screen and (max-width: '600px')`,
+  base: `screen and (max-width: '768px')`,
+  large: `screen and (max-width: '1024px')`,
+};
+
+const fontSize = {
+  xs: "0.5rem",
+  sm: "0.75rem",
+  base: "1rem",
+  md: "1.25rem",
+  lg: "1.5rem",
+  xl: "2rem",
+};
+
+const fontFamily = {
+  notoSans: "Noto Sans KR",
+  notoSerif: "Noto Serif KR",
+  rozha: "Rozha One",
+};
+
+const colorLight = {
+  background: "#FCFCFC",
+  primary: "#00E45B",
+  fontPrimary: "#2E2E2E",
+};
+
+const colorDark = {
+  background: "#2E2E2E",
+  primary: "#00E45B",
+  fontPrimary: "#FCFCFC",
+};
+
+export const theme: DefaultTheme = {
+  windowSize,
+  fontSize,
+  fontFamily,
+  colorDark,
+  colorLight,
+};
