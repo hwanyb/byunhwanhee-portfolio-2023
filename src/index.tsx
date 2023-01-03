c49d478074f633c1ab78c64b153242896271fe14
hwanyb <mnn9502@naver.com>
2023-01-03 16:00:00 +0900

    Feat : Set store, Provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,25 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyle from "./styles/GlobalStyle";
 import { ThemeProvider } from "styled-components";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
 import { theme } from "./styles/theme";
+import rootReducer from "./modules";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const store = createStore(rootReducer);
+
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Provider store={store}>
       <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
