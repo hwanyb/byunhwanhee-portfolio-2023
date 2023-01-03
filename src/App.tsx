e8a362984d598fff3707ab2f128eb386814ed9f5
hwanyb <mnn9502@naver.com>
2023-01-03 10:00:00 +0900

    Design : Add css App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import React from "react";
 import { Route, Routes } from "react-router";
 import { BrowserRouter } from "react-router-dom";
-import Header from "./components/common/Header";
+import styled from 'styled-components';
 
+import Header from "./components/common/Header";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 
+
+const Base = styled.div`
+  background-color: ${props => props.theme.colorLight.backgroud};
+`;
+
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-    </BrowserRouter>
+    <Base>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </BrowserRouter>
+    </Base>
   );
 }
 
