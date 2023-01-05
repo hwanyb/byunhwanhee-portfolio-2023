d4cce05305909e8d1eb3608f0ba323bfdad659e5
hwanyb <mnn9502@naver.com>
2023-01-05 11:00:00 +0900

    Design : Add css Project.tsx

--- src/components/Home/Project.tsx
+++ src/components/Home/Project.tsx
@@ -1,16 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Swiper, SwiperSlide } from "swiper/react";
-
 import "swiper/css";
+
 import ProjectItem from "./Project/ProjectItem";
 import projectData from "../../project.json";
 
 const Base = styled.div``;
-const StyledSwiper = styled(Swiper)``;
-const StyledSwiperSlide = styled(SwiperSlide)``;
+const StyledSwiper = styled(Swiper)`
+  width: 100vw;
+  padding: 0;
+  transition: all 0.5s ease-in-out;
+  & .swiper-slide-active {
+    transition: all 0.5s ease-in-out;
+    opacity: 1;
+    height: 600px;
+    @media screen and (max-width: 400px) {
+      height: 500px;
+    }
+  }
+  @media ${(props) => props.theme.windowSize.laptop} {
+    padding: 0 150px;
+  }
+  @media ${(props) => props.theme.windowSize.tablet} {
+    padding: 0 50px;
+  }
+  @media ${(props) => props.theme.windowSize.mobile} {
+    height: 500px;
+  }
+`;
+const StyledSwiperSlide = styled(SwiperSlide)`
+  transition: all 0.5s ease-in-out;
+  height: 550px;
+  margin: auto;
+  opacity: 0.7;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  &.swiper-slide-active:hover .info {
+    transition: all 0.5s ease-in-out;
+    opacity: 1;
+    visibility: visible;
+    left: 0;
+  }
+`;
 
 export default function Project() {
+  console.log(projectData);
   return (
     <Base>
       <StyledSwiper
