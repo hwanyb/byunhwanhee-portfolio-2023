9d909c83c9e07818836559a9528348e55002f814
hwanyb <mnn9502@naver.com>
2023-01-05 10:00:00 +0900

    Feat : Code Project.tsx - 프로젝트 swiper로 구현 - SlideItem Project.json map

--- src/components/Home/Project.tsx
+++ src/components/Home/Project.tsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import ProjectItem from "./Project/ProjectItem";
+import projectData from "../../project.json";
+
+const Base = styled.div``;
+const StyledSwiper = styled(Swiper)``;
+const StyledSwiperSlide = styled(SwiperSlide)``;
 
 export default function Project() {
-    return (
-        <div>Project</div>
-    )
+  return (
+    <Base>
+      <StyledSwiper
+        className="mySwiper"
+        slidesPerView={1}
+        loop={true}
+        centeredSlides={true}
+        spaceBetween={50}
+        breakpoints={{
+          768: {
+            spaceBetween: 150,
+            slidesPerView: 1,
+          },
+          1024: {
+            spaceBetween: 200,
+            slidesPerView: 2,
+          },
+          1600: {
+            spaceBetween: 300,
+            slidesPerView: 2.8,
+          },
+        }}
+      >
+        {projectData.map((data) => (
+          <StyledSwiperSlide key={data.id}>
+            <ProjectItem {...data} />
+          </StyledSwiperSlide>
+        ))}
+      </StyledSwiper>
+    </Base>
+  );
 }
